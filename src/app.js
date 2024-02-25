1d7745a284a36fff10518a998018057e17aba6e3
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-02-24 22:46:17 -0500

    fronted added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("../swagger_output.json");
+const path = require("path");
 
 dotenv.config({
   path: ".env",
@@ -20,7 +21,7 @@ const authRouter = require("./routes/auth-router");
 
 const app = express();
 
-app.use(
+/* app.use(
   cors({
     credentials: true,
     origin: [
@@ -31,7 +32,7 @@ app.use(
     ],
     methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
   })
-);
+); */
 
 app.use(
   process.env.DEVELOP_MODE === "prod"
@@ -43,10 +44,16 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
-app.use("/notes", noteRouter);
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/notes", noteRouter);
+app.use("/api/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/", express.static("src/public/"));
+
+app.get("*", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(400).json({ message: "route not found" });
---
+++ src/public/assets/cache-keys-ZmxRsDyf.js
@@ -0,0 +1,1 @@
+const o={NOTE_LIST:{ME:"notes",TRASH:"trash",NORMAL:"normal"},NOTE:"note"};export{o as C};
